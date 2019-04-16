532d029b946c774f8e4df7a18136f46e9a871482
seekingstar <stella.scraps@gmail.com>
2019-04-16 14:17:56 +0900

    UILabelでpaddingを設定できるようにする

---
+++ Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 清水 脩輔 on 2019/04/16.
+//  Copyright © 2019 清水 脩輔. All rights reserved.
+//
+
+import UIKit
+import SKPaddingLabel
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: SKPaddingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
